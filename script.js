1c4168eab811030654ebde7bcd2716dce21039d5
CodeNext <codenext@admins-MBP.lan>
2019-03-30 12:11:32 -0400

    ok

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-drop = document.getElementById("drop-down")
+function openMenu() {
+  var x = document.getElementById("dropdown");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function openProjects() {
+  var x = document.getElementById("projects");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
